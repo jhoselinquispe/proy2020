eb256e63e9b2fd0f1f8c4366455e2af480e5ca3a
jhomau <jhomau.666@gmail.com>
2020-10-03 20:38:37 -0400

    obtencion de objeto json (ruta de archivos) para empezar con android.

--- routes/service.js
+++ routes/service.js
@@ -16,22 +16,20 @@ router.get('/', function(req, res, next) {
 //SERVICIO PARA LOGIN
 router.post('/login', async (req,res)=>{
   var params = req.body;
-  //console.log(body);
-  if(params.Correo==""){
-    res.status(200).json({msn : "El Correo es Necesario para logear"});
+  if(params.Correo==null){
+    res.status(300).json({msn : "El Correo es Necesario para logear"});
     return;
   }
-  if(params.Password==""){
-    res.status(200).json({msn : "Es necesario introducir su password"});
+  if(params.Password==null){
+    res.status(300).json({msn : "Es necesario introducir su password"});
     return;
   }
   var result = await CLIENTE.find({Correo: params.Correo, Password: sha1(params.Password)});
   if(result.length == 1){
-    res.status(200).json({msn:"Bienvenido al Sistema"});
+    res.status(200).json({msn:"Bienvenido al Sistema "});
     return;
-  } else {
-    res.status(200).json({msn:"Credenciales incorrectas"});
   }
+  res.status(300).json({msn:"Credenciales incorrectas"});
 });
 
 //SERVICIO PARA GUARDAR IMAGEN PRUEBA
@@ -45,40 +43,27 @@ var path = __dirname.replace(/\/routes/g,("/img"));
 var date = new Date();
 var sign =  sha1(date.toString().substr(1,5));
 var pathMenu = path + "/" + sign + "_" + imagen.name.replace(/\s/g,"_");
-var obj = {};
-imagen.mv(pathMenu, async (err) =>{
+imagen.mv(pathMenu,(err) =>{
   if(err){
     return res.status(300).send({msn : "Error al subir la iamgen al servidor."})
   }
-  obj['Fotoproducto'] = pathMenu.toString();
-  var menusDB = await new MENUS (obj);
-  menusDB.save().then((err,docs)=>{
-    if(err){
-      res.status(500).json({msn: "ERROR EN LA BASE DE DATOS"});
-    }
-    res.status(200).json({msn: "Imagen de MENU registrado"});
-    });
+  var ruta = pathMenu.toString();
+  res.status(200).json({"rutaLogo": ruta});
   });
 });
+
 router.post('/imglogo',(req,res)=>{
   var imagen = req.files.image;
   var path = __dirname.replace(/\/routes/g,("/imgLogo"));
   var date = new Date();
   var sign =  sha1(date.toString().substr(1,5));
   var pathRestaurant = path + "/" + sign + "_" + imagen.name.replace(/\s/g,"_");
-  var obj = {};
-  imagen.mv(pathRestaurant, async (err) =>{
+  imagen.mv(pathRestaurant,(err) =>{
     if(err){
       return res.status(300).send({msn : "Error al subir la iamgen al servidor."})
     }
-    obj['Logo'] = pathRestaurant.toString();
-    var restaurantDB = await new RESTAURANT (obj);
-    restaurantDB.save().then((err,docs)=>{
-      if(err){
-        res.status(500).json({msn: "ERROR EN LA BASE DE DATOS"});
-      }
-      res.status(200).json({msn: "Imagen de MENU registrado"});
-      });
+    var rutaLogo = pathRestaurant.toString();
+    res.status(200).json({"rutaLogo": rutaLogo});
     });
   });
 module.exports = router;
